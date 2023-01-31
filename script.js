ce7ab83c960441b542e86969ca386e37f47ccbf3
João Lucas Migliorini <110692154+LucasMigliorini65@users.noreply.github.com>
2023-01-30 23:29:26 -0300

    Decodificador
    Upando decodificador

---
+++ script.js
@@ -0,0 +1,92 @@
+const textArea = document.querySelector(".text-area");
+const mensagem = document.querySelector(".mensagem");
+
+// As "chaves" de criptografia que utilizaremos são:
+// `A letra "e" é convertida para "enter"`
+// `A letra "i" é convertida para "imes"`
+// `A letra "a" é convertida para "ai"`
+// `A letra "o" é convertida para "ober"`
+// `A letra "u" é convertida para "ufat"`
+
+function btnEncriptar() {
+    const textoEncriptado = encriptar(textArea.value);
+    mensagem.value = textoEncriptado;
+    textArea.value = "";
+}
+
+console.log(mensagem)
+function encriptar(stringEncriptada) {
+
+    let matrizCodigo = [["e" , "enter"] , ["i" , "imes"], ["a" ,"ai"] , ["o" , "ober"], ["u" , "ufat"]];
+    stringEncriptada = stringEncriptada.toLowerCase();
+
+    for(let i = 0; i < matrizCodigo.length; i++) {
+        if(stringEncriptada.includes(matrizCodigo[i][0])) {
+            stringEncriptada = stringEncriptada.replaceAll(matrizCodigo[i][0], matrizCodigo[i][1]);
+        }
+    }
+
+    return stringEncriptada;
+}
+
+
+function btnDesencriptar() {
+    const textoDesencriptado = desencriptar(textArea.value);
+    mensagem.value = textoDesencriptado;
+    textArea.value = "";
+}
+
+
+function desencriptar(stringDesencriptada) {
+
+    let matrizCodigo = [["e" , "enter"] , ["i" , "imes"], ["a" ,"ai"] , ["o" , "ober"], ["u" , "ufat"]];
+    stringDesencriptada = stringDesencriptada.toLowerCase();
+
+    for(let i = 0; i < matrizCodigo.length; i++) {
+        if(stringDesencriptada.includes(matrizCodigo[i][1])) {
+            stringDesencriptada = stringDesencriptada.replaceAll(matrizCodigo[i][1], matrizCodigo[i][0]);
+        }
+    }
+
+    return stringDesencriptada;
+}
+
+
+document.querySelector('.btn-copiar').addEventListener('click', btnCopiar);
+
+function btnCopiar() {
+   document.querySelector('.mensagem').select();
+   document.execCommand('copy');
+
+}
+
+
+function verificar() {
+    if(document.querySelector(".mensagem").value === "") {
+      mensagem.style.backgroundImage= 'url("img/img-null.png")'
+        }
+
+    else {
+        mensagem.style.backgroundImage= 'url()'
+    }
+}
+
+setInterval(verificar, 10);
+
+
+
+
+function darkMode() {
+    var element = document.body;
+    element.classList.toggle("dark-mode");
+    var area = document.querySelector(".mensagem");
+    var area2 = document.querySelector(".text-area");
+    area.classList.toggle("area-dark");
+    area2.classList.toggle("area-dark");
+    var info = document.querySelector(".informacao");
+    info.classList.toggle("info-dark")
+
+ }
+
+ 
+   
